2779d7bc8a7c63600522981b9687f7e68a1ec8b7
Shoaib! <shoaibsheikh@gmail.com>
2022-03-02 17:44:20 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 # dollar-dotpyc.com
 ```
 Extension to Gnome-Shell in versions (38, 40, 41 or >), for conversion dollar USD in real BRL.
+=======
+# dollar-pkr
+```
+Extension to Gnome-Shell in versions (36,38,40,41), for conversion dollar USD in Rs PKR.
+>>>>>>> 6cb749d (initial commit)
 ```
 
 # Licence
@@ -22,6 +28,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
 
 # How to install
 ```
+<<<<<<< HEAD
 cd /tmp && git clone https://github.com/shoaibzs/dollar-dotpyc.com.git && mv dollar-dotpyc.com dollar@dotpyc.com && cp -av dollar@dotpyc.com ~/.local/share/gnome-shell/extensions/ && gnome-shell-extension-tool --enable-extension dollar@dotpyc.com && rm -rf dollar@dotpyc.com
 ```
 To restart GNOME Shell in X11, pressing Alt+F2 to open the Run Dialog and enter restart (or just r).
@@ -29,3 +36,8 @@ To restart GNOME Shell in X11, pressing Alt+F2 to open the Run Dialog and enter 
 # Preview
 ![image](https://user-images.githubusercontent.com/57331815/139193026-c7a321e5-0ba2-45e4-85dc-d1924045f43a.png)
 
+=======
+cd /tmp && git clone https://github.com/shoaibzs/dollar-pkr.git && mv dollar-pkr dollar-pkr@shoaibzs.github.com && cp -av dollar-pkr@shoaibzs.github.com ~/.local/share/gnome-shell/extensions/ && gnome-shell-extension-tool --enable-extension dollar-pkr@shoaibzs.github.com && rm -rf dollar-pkr@shoaibzs.github.com
+```
+To restart GNOME Shell in X11, pressing Alt+F2 to open the Run Dialog and enter restart (or just r). In Wayland Log out and Login agaian.
+>>>>>>> 6cb749d (initial commit)
--- extension.js
+++ extension.js
@@ -28,7 +28,11 @@ const PanelMenu = imports.ui.panelMenu;
 let panelButton;
 let panelButtonText;
 let _httpSession;
+<<<<<<< HEAD
 let _dollarQuotation;
+=======
+let _dollarQuatation;
+>>>>>>> 6cb749d (initial commit)
 let sourceId = null;
 
 // Start application
@@ -78,14 +82,22 @@ function load_json_async(){
 
     let message = Soup.form_request_new_from_hash(
         'GET', 
+<<<<<<< HEAD
         "http://economia.awesomeapi.com.br/json/last/USD-BRL", 
+=======
+        "http://economia.awesomeapi.com.br/json/last/USD-PKR", 
+>>>>>>> 6cb749d (initial commit)
         {});
     
     _httpSession.queue_message(message, () => {
         try {
             if (!message.response_body.data) {
                 panelButtonText = new St.Label({
+<<<<<<< HEAD
                     text : "(USD: 1,00) = (BRL: " + _dollarQuotation + ")" + " * ",
+=======
+                    text : "Not content body",
+>>>>>>> 6cb749d (initial commit)
                     y_align: Clutter.ActorAlign.CENTER,
                 });
                 panelButton.set_child(panelButtonText);
@@ -94,12 +106,21 @@ function load_json_async(){
             }
 
             let jp = JSON.parse(message.response_body.data);
+<<<<<<< HEAD
             _dollarQuotation = jp["USDBRL"]["bid"];
             _dollarQuotation = _dollarQuotation.split(".");
             _dollarQuotation = _dollarQuotation[0] + "," + _dollarQuotation[1].substring(0,2);
    
             panelButtonText = new St.Label({
                 text : "(USD: 1,00) = (BRL: " + _dollarQuotation + ")",
+=======
+            _dollarQuatation = jp["USDPKR"]["bid"];
+            _dollarQuatation = _dollarQuatation.split(".");
+            _dollarQuatation = _dollarQuatation[0] + "." + _dollarQuatation[1].substring(0,2);
+   
+            panelButtonText = new St.Label({
+                text : "[USD: 1.00 = PKR: " + _dollarQuatation + "]",
+>>>>>>> 6cb749d (initial commit)
                 y_align: Clutter.ActorAlign.CENTER,
             });
 
@@ -109,7 +130,11 @@ function load_json_async(){
 
         } catch (e) {
             panelButtonText = new St.Label({
+<<<<<<< HEAD
                 text : "(USD: 1,00) = (BRL: " + _dollarQuotation + ")" + " * ",
+=======
+                text : "Request error",
+>>>>>>> 6cb749d (initial commit)
                 y_align: Clutter.ActorAlign.CENTER,
             });
 
